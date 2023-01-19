8b159944c0797934002b78252d847800e3d9cd70
Renan Cascarano de Souza <30605188+canaldorenan2@users.noreply.github.com>
2023-01-19 09:38:12 -0300

    2 - Tilesheets, change player
    Creation 90%
    Game 20%

--- Assets/Scripts/MenuCreationPlayer.cs
+++ Assets/Scripts/MenuCreationPlayer.cs
@@ -1,38 +1,139 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuCreationPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject nextButtom;
+    public GameObject previousButtom;
 
-    // Update is called once per frame
-    void Update()
+    public GameObject gameObjectHat;
+    public GameObject gameObjectHair;
+    public GameObject gameObjectBeard;
+    public GameObject gameObjectFace;
+
+    SpriteRenderer hats;
+    SpriteRenderer hair;
+    SpriteRenderer beard;
+    SpriteRenderer face;
+
+    public Sprite[] spritesHat;
+    public Sprite[] spritesHair;
+    public Sprite[] spritesBeard;
+    public Sprite[] spritesFace;
+
+    int count = 0;
+
+
+
+    private void Start()
     {
-        
+        hats = gameObjectHat.GetComponent<SpriteRenderer>();
+        hair = gameObjectHair.GetComponent<SpriteRenderer>();
+        beard = gameObjectBeard.GetComponent<SpriteRenderer>();
+        face = gameObjectFace.GetComponent<SpriteRenderer>();
     }
 
     public void Next()
     {
-        Debug.Log("Next");
+        if (count == spritesHat.Length - 1)
+            count = 0;
+        else
+            count++;
+
+        int buttomFunction = GetButtomFunction();
+
+        ChangeSkinCollor(buttomFunction);
     }
 
     public void Back()
     {
-        Debug.Log("Back");
+        if (count == 0)
+            count = spritesHat.Length - 1;
+        else
+            count--;
+
+        int buttomFunction = GetButtomFunction();
+
+        ChangeSkinCollor(buttomFunction);
     }
 
     public void NextStep()
     {
-        Debug.Log("NextStep");
+        SceneManager.LoadScene("MainGame"); ;
     }
 
     public void Exit()
     {
-        Application.Quit();
+        if (SceneManager.GetActiveScene().name == "PlayerCreation")
+        {
+            Application.Quit();
+        }
+        else
+        {
+            SceneManager.LoadScene("MainGame");
+        }
+
+
+    }
+
+    int GetButtomFunction()
+    {
+        string bName;
+        int buttomFunction = 0;
+
+        bName = gameObject.name;
+
+        buttomFunction = (int)char.GetNumericValue(bName[0]);
+
+        return buttomFunction;
+    }
+
+    void ChangeSkinCollor(int function)
+    {
+        switch (function)
+        {
+            case 0:
+                hats.sprite = spritesHat[count];
+                break;
+            case 1:
+                hair.sprite = spritesHair[count];
+                break;
+            case 2:
+                beard.sprite = spritesBeard[count];
+                break;
+            case 3:
+                face.sprite = spritesFace[count];
+                break;
+
+            default:
+                //
+                break;
+
+        }
+
+    }
+
+    public void ChangeButtomFunction()
+    {
+        // 0 - Change Hat, 1 - Change Hair, 2 - Change Beard, 3 - Change Face
+
+        int newNumber = 0;
+
+        newNumber = (int)char.GetNumericValue(nextButtom.name[0]);
+
+        if (newNumber >= 4)
+        {
+            newNumber = 0;
+        }
+        else
+        {
+            newNumber++;
+        }
+
+        nextButtom.name = "" + newNumber + " - Next - Skin";
+        previousButtom.name = "" + newNumber + " - Back - Skin";
+
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Dash
- 
+Assets used in this project
+
+From unity assets:
+Miniature Army 2D V.1 [Medieval Style]
+SunnyLand Expansion Pack Trees
+FREE Stylized PBR Textures Pack
+Customizable skybox
+2d game kit
+Free game items
+Painted HQ 2D Forest Medieval Background
+2D SD Character Pack V.1
+
+https://shubibubi.itch.io/cozy-farm:
+cozy farm
+
+Freepick:
+Pop-ups
+
+Music:
+https://www.youtube.com/watch?v=UHR6y7faVJM&ab_channel=Kato
